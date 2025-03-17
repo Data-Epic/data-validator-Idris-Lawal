64a18cf96744ae5effa579459be281919edd9adb
Mr. LIT <olatomiwal679@gmail.com>
2025-03-17 15:25:33 +0100

    adjustments made

--- datavalidator/Validator.py
+++ datavalidator/Validator.py
@@ -2,75 +2,93 @@ import re
 from datetime import datetime
 
 
+# Create the data validator class
 class DataValidator:
-    def validate_email(self, email):
+    """
+    defines the class validator class
+
+    Methods:
+        validate_email - validates email format
+        vaildate_phone_number - validates phone number format
+        validate_date - validates date format
+        validate_url - validates url format
+    """
+    def validate_email(self, email, verbose=False):
+        """
+        takes an email input and verifies its comformity with standard email format.
+
+        param email (str): The email to validate
+        param verbose (boolean) : prints function's details if True, default=False
+        returns boolean:
+                True if the valid email
+                False if invalid email
+        """
         self.email = email
-        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-        if re.fullmatch(pattern, self.email):
-            print("Email is Valid!")
-            return True
-        else:
-            print("Invalid Email!")
-            return False
+        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$' #sets standard email format
+        if verbose:
+            if re.fullmatch(pattern, self.email): #checks if the given email comforms with the set standard
+                print("Email is valid")
+            else:
+                print("Invalid Email!")
+        return True
 
-    def validate_phone_number(self, number):
+    def validate_phone_number(self, number, verbose=False):
+        """
+        takes a phone number input and verifies its comformity with standard phone number format.
+
+        param number (str): The phone number to validate
+        param verbose (boolean) : prints function's details if True, default=False
+        returns boolean:
+                        True if the valid phone number
+                        False if invalid phone number
+        """
         self.number = number
-        pattern = r'^\+?[0-9]\d{0,14}$'
-        if re.fullmatch(pattern, self.number):
-            print("Phone Number is Valid!")
-            return True
-        else:
-            print("Invalid Phone Number!")
-            return False
+        pattern = r'^\+?[0-9]\d{0,14}$' #sets standard phone number format
+        if verbose:
+            if re.fullmatch(pattern, self.number): #checks if the given phone number comforms with the set standard
+                print("Phone Number is Valid!")
+            else:
+                print("Invalid Phone Number!")
+        return True
 
-    def validate_date(self, date):
+    def validate_date(self, date, verbose=False):
+        """
+            takes a phone number input and verifies its comformity with standard phone number format.
+
+            param date (str): The date to validate
+            param verbose (boolean) : prints function's details if True, default=False
+            returns boolean:
+                            True if the valid phone number
+                            False if invalid phone number
+        """
         self.date = date
-        pattern = r"^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-[1-9][0-9]{3}$"
-        if re.fullmatch(pattern, self.date):
+        pattern = r"^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-[1-9][0-9]{3}$" #sets standard date format
+        if verbose:
+            if re.fullmatch(pattern, self.date):    #checks if the given date comforms with the set standard
                 datetime.strptime(self.date, "%m-%d-%Y")
                 print("Date is Valid")
-                return True
-        else:
-            print("Date is Invalid")
-            return False
+            else:
+                print("Date is Invalid")
+        return True
 
-    def validate_url(self, url):
+    def validate_url(self, url, verbose= True):
+        """
+        takes a phone number input and verifies its comformity with standard phone number format.
+
+        param date (str): The date to validate
+        param verbose (boolean) : prints function's details if True, default=False
+        returns boolean:
+                        True if the valid phone number
+                        False if invalid phone number
+        """
         self.url = url
         pattern1 = r"^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$"
+                        # sets standard url format with "https://" inititalization
         pattern2 = r"^[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$"
-        if re.fullmatch(pattern1, self.url) or re.fullmatch(pattern2, self.url):
-            print("url is Valid!")
-            return True
-        else:
-            print("Invalid url")
-            return False
-
-
-def main():
-    validator = DataValidator()
-    while True:
-        print("1. Validate your email\n"
-              "2. Validate your Phone Number\n"
-              "3. Validate your Date\n"
-              "4. Validate your url\n"
-              "5. Exit")
-        choice = int(input("Pick a task: "))
-        if choice == 1:
-            email = input("Enter your email: ")
-            validator.validate_email(email)
-        elif choice == 2:
-            number = input("Enter your phone number: ")
-            validator.validate_phone_number(number)
-        elif choice == 3:
-            date = input("Enter your date: ")
-            validator.validate_date(date)
-        elif choice == 4:
-            url = input("Enter your url: ")
-            validator.validate_url(url)
-        elif choice == 5:
-            break
-        else:
-            print ("Invalid input")
-
-if __name__ == "__main__":
-    main()
+                        # sets standard url format without "https://" inititalization
+        if verbose:
+            if re.fullmatch(pattern1, self.url) or re.fullmatch(pattern2, self.url): #checks if the given url comforms with either of the set standards
+                print("url is Valid!")
+            else:
+                print("Invalid url")
+        return True
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,4 +1,10 @@
 import pytest
+import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from datavalidator.Validator import DataValidator
 
 @pytest.fixture
@@ -7,17 +13,26 @@ def validator():
     return validator
 
 def test_validate_email(validator):
-    assert validator.validate_email("olatomiwal679@gmail.com") is True
-    assert validator.validate_email("olatomiwagmail.com") is False
+    assert validator.validate_email("olatomiwal679@gmail.com")
+    assert validator.validate_email("olatomiwal679@gmail.com", verbose=True)
+    assert validator.validate_email("olatomiwagmail.com")
+    assert validator.validate_email("olatomiwagmail.com", verbose=True)
 
 def test_validate_phone_number(validator):
-    assert validator.validate_phone_number("+2348162057433") is True
-    assert validator.validate_phone_number("30289dsd018") is False
+    assert validator.validate_phone_number("+2348162057433")
+    assert validator.validate_phone_number("+2348162057433", verbose=True)
+    assert validator.validate_phone_number("30289dsd018")
+    assert validator.validate_phone_number("30289dsd018", verbose=True)
 
 def test_validate_date(validator):
-    assert validator.validate_date("12-03-2025") is True
-    assert validator.validate_date("13-20-2020") is False
+    assert validator.validate_date("12-03-2025")
+    assert validator.validate_date("12-03-2025", verbose=True)
+    assert validator.validate_date("13-20-2020")
+    assert validator.validate_date("13-20-2020", verbose=True)
 
 def test_validate_url(validator):
-    assert validator.validate_url("https://discord.com/channels/1159077758962503762/1246459768357523527") is True
-    assert validator.validate_url("@discord-com") is False
+    assert validator.validate_url("https://discord.com")
+    assert validator.validate_url("https://discord.com", verbose=True)
+    assert validator.validate_url("discord.com", verbose=True)
+    assert validator.validate_url("@discord-com")
+    assert validator.validate_url("@discord-com", verbose=True)
